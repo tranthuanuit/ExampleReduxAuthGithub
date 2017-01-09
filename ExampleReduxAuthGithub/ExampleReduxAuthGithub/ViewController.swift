4d583204c2a49beed9320a921159db78ae26dea8
Thuan Tran <tranthuan90.uit@gmail.com>
2017-01-09 18:28:59 +0700

    init project

---
+++ ExampleReduxAuthGithub/ExampleReduxAuthGithub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExampleReduxAuthGithub
+//
+//  Created by Thuan Tran on 1/9/17.
+//  Copyright © 2017 Thuan Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
